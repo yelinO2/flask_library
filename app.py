9b6ee027e7f274a14f42829f3a30b1b1c8ee5ac4
yelinO2 <yelinoo935@gmail.com>
2022-10-27 17:28:05 +0630

    add comments

--- app.py
+++ app.py
@@ -18,21 +18,21 @@ api = Api(app)
 app.secret_key = "not_today"
 JWT = JWT(app, authenticate, identity)
 
-api.add_resource(UserRegister, '/register') 
-api.add_resource(UserList, '/all_users')
-api.add_resource(Books, '/book/add')
-api.add_resource(BookList, '/books')
-api.add_resource(SearchByLang, '/books/lang/<string:lang>')
-api.add_resource(SearchByBookName, '/books/<string:name>')
+api.add_resource(UserRegister, '/register') # user register
+api.add_resource(UserList, '/all_users') # user list
+api.add_resource(Books, '/book/add') # add books
+api.add_resource(BookList, '/books') # book list
+api.add_resource(SearchByLang, '/books/lang/<string:lang>') # search book by lang
+api.add_resource(SearchByBookName, '/books/<string:name>') # search book by name
 # api.add_resource(UpdateBookInfo, '/book/update')
-api.add_resource(DeleteBook, '/book/delete/<int:b_id>')
-api.add_resource(Authors, '/author/add')
-api.add_resource(Genre, '/genre/add')
-api.add_resource(AuthorList, '/authors/all')
-api.add_resource(GenreList, '/genre/all')
-api.add_resource(SearchGenre, '/genre/<string:genre>')
-api.add_resource(SearchAuthor, '/author/<string:author>')
-
+api.add_resource(DeleteBook, '/book/delete/<int:b_id>') # delete book
+api.add_resource(Authors, '/author/add') # add author
+api.add_resource(Genre, '/genre/add')  # add genre
+api.add_resource(AuthorList, '/authors/all') # author list
+api.add_resource(GenreList, '/genre/all') #  genre list
+api.add_resource(SearchGenre, '/genre/<string:genre>') # search by genre name
+api.add_resource(SearchAuthor, '/author/<string:author>') # search by author name
+ 
 @app.before_first_request
 def create_table():
     db.create_all()
